b8df850036d0d08a749f207bc272b290b259f1db
Yonatan Kra <yonatan.kr@walkme.com>
2018-01-02 09:36:58 +0200

    Cleanup config

--- webpack.common.js
+++ webpack.common.js
@@ -18,17 +18,6 @@ module.exports = {
             jQuery: 'jquery',
             $: 'jquery',
             jquery: 'jquery'
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "vendor",
-            minChunks: function (module) {
-                return module.context &&
-                    module.context.includes("node_modules");
-            }
-        }),
-        new webpack.optimize.CommonsChunkPlugin({
-            name: "manifest",
-            minChunks: Infinity
         })
     ],
     module: {
